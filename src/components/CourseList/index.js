93c509f058dc786624677cc8023a9f5454275250
vinicius rodrigues <vinitecnico@gmail.com>
2019-11-26 21:49:02 -0300

    first commit

---
+++ src/components/CourseList/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function CourseList() {
+    const courses = useSelector(state => state.data);
+    const dispatch = useDispatch();
+
+    function addCourse() {
+        dispatch({ type: 'ADD_COURSE', title: 'GraphQL' });
+    }
+
+    return (
+        <div>
+            <ul>
+                {courses.map(course => <li key={course}>{course}</li>)}
+            </ul>
+            <button type="button" onClick={addCourse}>
+                addCourse
+            </button>
+        </div>
+    );
+}
